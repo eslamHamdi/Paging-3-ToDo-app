500674c73919f0e06f7794b054d3caec752bba3b
eslamHamdi <eslam.hamdi.hassan@gmail.com>
2021-07-02 23:43:51 +0200

    first commit

---
+++ app/src/main/java/com/richarddewan/paging3_todo/data/remote/response/TaskResponse.kt
@@ -0,0 +1,74 @@
+package com.richarddewan.paging3_todo.data.remote.response
+
+import android.os.Parcelable
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+
+@Keep
+data class TaskResponse(
+
+	@field:SerializedName("first_page_url")
+	val firstPageUrl: String? = null,
+
+	@field:SerializedName("path")
+	val path: String? = null,
+
+	@field:SerializedName("per_page")
+	val perPage: Int? = null,
+
+	@field:SerializedName("total")
+	val total: Int? = null,
+
+	@field:SerializedName("data")
+	val data: List<DataItem?>? = null,
+
+	@field:SerializedName("last_page")
+	val lastPage: Int? = null,
+
+	@field:SerializedName("last_page_url")
+	val lastPageUrl: String? = null,
+
+	@field:SerializedName("next_page_url")
+	val nextPageUrl: String? = null,
+
+	@field:SerializedName("from")
+	val from: Int? = null,
+
+	@field:SerializedName("to")
+	val to: Int? = null,
+
+	@field:SerializedName("prev_page_url")
+	val prevPageUrl: Any? = null,
+
+	@field:SerializedName("current_page")
+	val currentPage: Int? = null
+)
+
+@Keep
+data class DataItem(
+
+	@field:SerializedName("note")
+	val note: String? = null,
+
+	@field:SerializedName("updated_at")
+	val updatedAt: String? = null,
+
+	@field:SerializedName("user_id")
+	val userId: String? = null,
+
+	@field:SerializedName("created_at")
+	val createdAt: String? = null,
+
+	@field:SerializedName("id")
+	val id: String? = null,
+
+	@field:SerializedName("title")
+	val title: String? = null,
+
+	@field:SerializedName("body")
+	val body: String? = null,
+
+	@field:SerializedName("status")
+	val status: String? = null
+)
